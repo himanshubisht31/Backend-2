004501528e85520dcba646cbdf7d15e041fccb4c
Himanshu <himanshubisht3112@gmail.com>
2021-11-08 16:02:24 +0530

    6th

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+function verifyToken(token) {
+    return new Promise((res, rej) => {
+        jwt.verify(token, process.env.KEY, (error, similar) => {
+            if(error) return rej(error)
+
+            else return res(similar);
+        })
+    })
+}
+
+async function authenticate(req, res, next) {
+    const bearerToken = req.headers.authorization
+    
+    if (!bearerToken || !bearerToken.startsWith('Bearer')) return res.status(400).send({ message: "Bearer token missing" })
+    
+    const token = bearerToken.split(' ')[1];
+
+    try {
+        const {user}=await verifyToken(token)
+        req.user=user
+        return next()
+        
+    } catch (error){ return res.status(400).send({message:'Invalid Token'})}
+        
+    
+}
--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,20 +1,31 @@
-const mongoose = require('mongoose')
-const bcryptjs=require('bcryptjs')
+const mongoose = require("mongoose");
+const bcryptjs = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true},
-    password:{type:String,required:true},
-    profile_photo_url:{type:String,required:true},
-    roles:[{type:String}]
-}, {
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    profile_photo_url: { type: String, required: true },
+    roles: [{ type: String }],
+  },
+  {
     versionKey: false,
-    timestamps:true
-})
+    timestamps: true,
+  }
+);
 
-userSchema.pre('save', function (next) {
-    
-})
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
 
+  const hashedPassword = bcryptjs.hashSync(this.password, 8);
+  this.password = hash;
+  return next();
+});
 
-module.exports=new mongoose.model('user',userSchema)
+userSchema.methods.checkPassword = function (password) {
+    const matchedPassword=bcryptjs.compareSync(password,this.password)
+    return matchedPassword
+}
+
+module.exports = new mongoose.model("user", userSchema);
