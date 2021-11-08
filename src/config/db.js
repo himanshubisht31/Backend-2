8707a10bb65d224257aed78c2b75fe9585df68b4
Himanshu <himanshubisht3112@gmail.com>
2021-11-08 17:01:52 +0530

    9th

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 const connect = () => {
-    return mongoose.connect("mongodb://127.0.0.1:27017/Backend_Evaluation");
+    return mongoose.connect("mongodb://127.0.0.1:27017/Backend_Evaluation_2");
 }
 
 module.exports=connect
